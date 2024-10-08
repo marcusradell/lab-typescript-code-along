4b8f5fb18b2132c57614538049f483d3aeb4dd5c
Marcus Rådell <marcus@radell.net>
2024-10-08 15:35:54 +0200

    upper case names works with empty array

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,22 @@
 import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 
-test("It works!", () => {
-  deepEqual(1, 1);
+// const data = [
+//   { id: "964465c7-ee20-40bf-9b5a-96631799c731", name: "Elton Johnson" },
+//   { id: "508d1603-6a96-48ea-ab14-c0e9bc0f710d", name: "Id Po" },
+// ];
+
+type Person = {
+  id: string;
+  name: string;
+};
+
+function uppercaseNames(names: Person[]) {
+  return [];
+}
+
+test("UpperCaseNames works with empty array", () => {
+  const result = uppercaseNames([]);
+
+  deepEqual(result, []);
 });
